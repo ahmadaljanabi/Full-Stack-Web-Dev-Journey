39a3b31f788bd827293136b1908d11953a742e28
احمد الجنبي <ahmad-3434@hotmail.com>
2022-06-15 16:06:57 +0300

    To Do List version 2 (Add database to it)

---
+++ todolist-v2-starting-files/app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+// Require Mongoose after install it in command line (npm i mongoose)
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Connect to mongoose and define the Database name you want to create
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+// Create a schema sctructure for the data you will create later
+const itemsSchema = {
+  name: String
+};
+
+// Create a model with a singular form of that schema
+const Item = mongoose.model("Item", itemsSchema);
+
+// Create a value by calling the model you created
+const item1 = new Item({
+  name: "Web Development",
+});
+
+const item2 = new Item({
+  name: "CCNA",
+});
+
+const item3 = new Item({
+  name: "Spanish & Japanese"
+});
+
+// Collect items in an array
+const defaultItems = [item1, item2, item3];
+
+// Using model name and insertMany function for inserting data you created to the Database
+Item.insertMany(defaultItems, function(err) {
+  // Create the logic if there is an error
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully added items to the database");
+  }
+})
+
+app.get("/", function(req, res) {
+
+
+
+  res.render("list", {listTitle: "Today", newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
