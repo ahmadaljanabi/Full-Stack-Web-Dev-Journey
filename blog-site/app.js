5cc2d3c197f239dfa51730948e6e8f0362b0e9b3
ahmadaljanabi <ahmad-3434@hotmail.com>
2022-06-21 01:03:18 +0300

    Create Database to blog-site

--- blog-site/app.js
+++ blog-site/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,6 +18,25 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const blogsSchema = {
+  title: String,
+  author: String,
+  body: String,
+  comments: [{body: String, date: Date}],
+  date: {type: Date, default: Date.now},
+};
+
+const Blog = mongoose.model('Blog', blogsSchema);
+
+const blog1 = new Blog({
+  title: "CCNA Review",
+  author: "Ahmed Aljanabi",
+  body: "Review to Cisco Certified Network Associated",
+  comments: "Nice!"
+});
 
 app.get("/", function(req, res) {
   res.render("home", {
