fc7477706cfec7f1d085b8d82f28ef8d1e117e27
احمد الجنبي <ahmad-3434@hotmail.com>
2022-06-25 15:35:14 +0300

    Blog-site + DB Completed

--- blog-site/app.js
+++ blog-site/app.js
@@ -3,14 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
 
 const app = express();
 
@@ -21,84 +18,61 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-const blogsSchema = {
+const postSchema = {
   title: String,
-  author: String,
-  body: String,
-  comments: [{body: String, date: Date}],
-  date: {type: Date, default: Date.now},
+  content: String
 };
 
-const Blog = mongoose.model('Blog', blogsSchema);
+const Post = mongoose.model("Post", postSchema);
 
-const blog1 = new Blog({
-  title: "CCNA Review",
-  author: "Ahmed Aljanabi",
-  body: "Review to Cisco Certified Network Associated",
-  comments: "Nice!"
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 });
 
-app.get("/", function(req, res) {
-  res.render("home", {
-    homeInfo: homeStartingContent,
-    posts: posts
-    });
-});
-
-app.get("/about", function(req, res) {
-  res.render("about", {aboutInfo: aboutContent});
-});
-
-app.get("/contact", function(req, res) {
-  res.render("contact", {contactInfo: contactContent});
-});
-
-app.get("/compose", function(req, res) {
+app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
-
-  const post = {
+app.post("/compose", function(req, res){
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
 
-  res.redirect("/");
-
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function(req, res){
 
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
+const requestedPostId = req.params.postId;
 
-    if (requestedTitle === storedTitle) {
-
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-
-    }
-
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 
 });
 
-// Routing Parameters
-// app.get("/news/:topic", function(req, res) {
-//   console.log(req.params.topic);
-// });
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
